74eddd4f951c680e8d642de19073f569b7976d9c
coronaisme <coronaisme@gmail.com>
2020-06-04 11:51:41 -0700

    maxOf recursive

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Code your solution here!
+function maxOf(arr) {
+  if (arr.length <= 1) {
+    return arr[0];
+  } else {
+    arr[0] > arr[1] ? arr.splice(1, 1) : arr.splice(0, 1);
+    return maxOf(arr);
+  }
+}
 
